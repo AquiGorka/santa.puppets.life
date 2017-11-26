868a690a1970a8eb69d8bf4991e9a5c11637921e
Gorka <gorka@aquigorka.com>
2017-11-26 11:00:10 +0100

    Fix coding styles

--- webpack.config.js
+++ webpack.config.js
@@ -5,30 +5,30 @@ var webpack = require('webpack');
 var definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
   __PROD__: JSON.stringify(JSON.parse(process.env.BUILD_PROD || 'false')),
-	GOOGLE_APIKEY: JSON.stringify(process.env.GOOGLE_APIKEY),
-	KURT_URL: JSON.stringify(process.env.KURT_URL)
+  GOOGLE_APIKEY: JSON.stringify(process.env.GOOGLE_APIKEY),
+  KURT_URL: JSON.stringify(process.env.KURT_URL)
 });
 
 module.exports = {
   entry: [
-	'./src/index.js'
+    './src/index.js'
   ],
   output: {
-  	path: './public/js/',
+    path: './public/js/',
     filename: 'index.js'       
   },
   plugins: [
-  	definePlugin
+    definePlugin
   ],
   module: {
     loaders: [
       { test: /.js$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets:['react', 'es2015'] } },
-			{ test: /\.json$/, loader: 'json-loader' }
+      { test: /\.json$/, loader: 'json-loader' }
     ]
   },
-	node: {
-		fs: 'empty',
-		net: "empty",
+  node: {
+    fs: 'empty',
+    net: "empty",
     tls: "empty"
-	}
+  }
 };
